957f66ed7c22e68ebefdccf1d14e59f92b6f1314
Paul jeanroy <pauljeanroy@outlook.fr>
2024-06-25 16:13:47 +0200

    [PJ le 25/06] Gestion de l'affichage de la navigation selon le role de l'utilisateur

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,24 +6,51 @@
     - ...
     
 */
+import useMenuSelect from "../hooks/useMenuSelect";
+import { useUserRole } from "../hooks/useUserRole";
 import "../styles/Footer.css";
 
 export default function Footer() {
+    const { menuSelect, setMenuSelect } = useMenuSelect();
+    const { roleUser } = useUserRole();
+
     return (
         <footer className="footer">
             <div className="div_container_footer">
                 <h3 className="h3_titre_site">Livret d'apprentissage numérique</h3>
                 <div className="div_nav_footer">
                     <nav className="nav_footer">
+
                         <div className="div_nav_accueil_footer">
                             <a href={"/accueil"}>Accueil</a>
-                        </div>
-                        <div className="div_nav_livret">
-                            <a href={"/livret"}>Livret</a>
                         </div>
                         <div className="div_nav_profil">
                             <a href={"/profil"}>Profil</a>
                         </div>
+
+                        {roleUser == "coordinateur" && (
+                            <>
+                                <div className="div_nav_profil">
+                                    <a href={"/gestionUtilisateur"} onClick={() => setMenuSelect("gestionUtilisateur")} className={menuSelect == "gestionUtilisateur" ? "selected" : ""}>Gestion Utilisateur</a>
+                                </div>
+
+                                <div className="div_nav_profil">
+                                    <a href={"/gestionLivret"} onClick={() => setMenuSelect("gestionLivret")} className={menuSelect == "gestionLivret" ? "selected" : ""}>Gestion Livret</a>
+                                </div>
+
+                                <div className="div_nav_profil">
+                                    <a href={"/creationTitre"} onClick={() => setMenuSelect("creationTitre")} className={menuSelect == "gestionLivret" ? "selected" : ""}>Créér un Titre</a>
+                                </div>
+
+                            </>
+                        )}
+
+                        {(roleUser == "apprenti" || roleUser == "maître d'apprentissage") && (
+                            <div className="div_nav_livret_footer">
+                                <a href={"/livret"} onClick={() => setMenuSelect("livret")} className={menuSelect == "livret" ? "selected" : ""}>Livret</a>
+                            </div>
+                        )}
+
                     </nav>
                 </div>
                 <div className="div_pub_footer">
